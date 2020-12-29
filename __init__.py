c0df70c0bbd18b61d7184ce19525f3ef18559f5c
Kris Gesling <kris.gesling@mycroft.ai>
2020-12-29 20:30:42 +0930

    add prompt page

--- __init__.py
+++ __init__.py
@@ -16,14 +16,15 @@ class WifiConnect(MycroftSkill):
     def initialize(self):
         """Create event handlers"""
         self.add_event("mycroft.internet.connected", self.handle_internet_connected)
+        self.add_event("system.wifi.setup.hotspot_activated", self.prompt_to_join_ap)
         self.add_event(
-            "system.wifi.setup.hotspot_activated", self.handle_wifi_setup_started
+            "system.wifi.setup.hotspot_connected", self.prompt_to_sign_in_to_ap
         )
         self.add_event(
             "system.wifi.setup.network_selection",
-            self.handle_wifi_setup_network_selection,
+            self.prompt_to_select_network,
         )
-        self.add_event("system.wifi.setup.connected", self.handle_wifi_setup_connected)
+        self.add_event("system.wifi.setup.connected", self.report_setup_complete)
 
     @intent_handler("test.intent")
     def show_all_screens(self, message):
@@ -33,40 +34,63 @@ class WifiConnect(MycroftSkill):
         """
         from time import sleep
 
-        images = [
-            f"{self.root_dir}/ui/1_connect-to-ap.png",
-            f"{self.root_dir}/ui/2_follow-prompt.png",
-            f"{self.root_dir}/ui/3_choose-wifi.png",
+        # images = [
+        #     f"{self.root_dir}/ui/0_start.png",
+        #     f"{self.root_dir}/ui/1_connect-to-ap.png",
+        #     f"{self.root_dir}/ui/2_follow-prompt.png",
+        #     f"{self.root_dir}/ui/3_choose-wifi.png",
+        # ]
+        # for image in images:
+        #     self.log.info(image)
+        #     self.gui.show_image(image)
+        #     sleep(10)
+        steps = [
+            self.prompt_to_join_ap,
+            self.prompt_to_sign_in_to_ap,
+            self.prompt_to_select_network,
+            self.report_setup_complete
         ]
-        for image in images:
-            self.log.info(image)
-            self.gui.show_image(image)
-            sleep(10)
+        for step in steps:
+            step(None)
+            sleep(5)
+
 
     def handle_internet_connected(self, message):
         """System came online later after booting."""
         self.enclosure.mouth_reset()
 
-    def handle_wifi_setup_started(self, message):
+    def prompt_to_join_ap(self, message):
         """Provide instructions for setting up wifi."""
         text = self.translate("device.wifi.setup.started")
         self.speak_dialog(text)
-        image = f"{self.root_dir}/ui/1_connect-to-ap.png"
-        self.log.info(image)
-        self.gui.show_image(image)
+        self.gui["phone_image"] = "1_phone_connect-to-ap.png"
+        self.gui["prompt"] = "Connect to the \nWifi network"
+        self.gui["highlight"] = "MYCROFT"
+        self.gui.show_page("prompt.qml")
 
-    def handle_wifi_setup_network_selection(self, message):
+    def prompt_to_sign_in_to_ap(self, message):
+        """Provide instructions for setting up wifi."""
+        text = self.translate("device.wifi.setup.started")
+        self.speak_dialog(text)
+        self.gui["phone_image"] = "2_phone_follow-prompt.png"
+        self.gui["prompt"] = "Follow the \nprompt on your \nmobile device or \ncomputer"
+        self.gui["highlight"] = ""
+
+    def prompt_to_select_network(self, message):
         """Prompt user to select network and login."""
         text = self.translate("device.wifi.setup.network.selection")
         self.speak_dialog(text)
-        self.gui.show_text(text)
+        self.gui["phone_image"] = "3_phone_choose-wifi.png"
+        self.gui["prompt"] = "Choose the \nWifi network to \nconnect your \nMycroft device"
+        self.gui["highlight"] = ""
 
-    def handle_wifi_setup_connected(self, message):
+    def report_setup_complete(self, message):
         """Wifi setup complete, network is connected."""
         self.speak_dialog("device.wifi.setup.complete")
         self.gui["icon"] = "check-circle.svg"
         self.gui["label"] = "Connected"
         self.gui["bgColor"] = green
+        self.gui.remove_page("prompt.qml")
         self.gui.show_page("status.qml")
 
 
