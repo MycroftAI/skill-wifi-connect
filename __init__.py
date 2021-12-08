64a2d08fbc54e1824b896f3e9b4f8190ffb9b626
Michael Hansen <mike@rhasspy.org>
2021-12-08 13:10:28 -0500

    Use Dbus to detect hotspot events

--- __init__.py
+++ __init__.py
@@ -13,26 +13,30 @@
 # limitations under the License.
 #
 """Mycroft skill for joining a device to a WiFi network."""
+import asyncio
+import threading
 from time import sleep
 
 from mycroft.audio import stop_speaking
 from mycroft.identity import IdentityManager
 from mycroft.messagebus import Message
 from mycroft.skills import MycroftSkill, intent_handler
-from mycroft.util import connected
+from mycroft.util.network_utils import (
+    get_dbus,
+    get_network_manager,
+    NM_NAMESPACE,
+)
 
 
-MARK_II = 'mycroft_mark_2'
+MARK_II = "mycroft_mark_2"
 
-def has_paired_before() -> bool:
-    """Simple check for whether a device has previously been paired.
+# NetworkManager constants
+NM_DEVICE_TYPE_WIFI = 2
 
-    This does not verify that the pairing information is valid or up to date.
-    The assumption being - if it's previously paired, then it has previously
-    connected to the internet.
-    """
-    identity = IdentityManager.get()
-    return identity.uuid != ""
+NM_802_11_MODE_UNKNOWN = 0
+NM_802_11_MODE_INFRA = 2
+NM_802_11_MODE_AP = 3
+
 
 class WifiConnect(MycroftSkill):
     """Skill that joins a device to a WiFi network.
@@ -40,6 +44,7 @@ class WifiConnect(MycroftSkill):
     Attributes:
         page_showing: on a GUI enabled device, the page being displayed
     """
+
     def __init__(self):
         super().__init__()
         self.page_showing = None
@@ -47,49 +52,40 @@ class WifiConnect(MycroftSkill):
 
     @property
     def platform(self):
-        return self.config_core['enclosure'].get('platform', 'unknown')
+        return self.config_core["enclosure"].get("platform", "unknown")
 
     def initialize(self):
         """Create event handlers"""
-        # TODO wire up message bus events to trigger prompts.
-        # self.add_event("system.wifi.setup.hotspot_activated", self.prompt_to_join_ap)
-        # self.add_event(
-        #     "system.wifi.setup.hotspot_connected", self.prompt_to_sign_in_to_ap
-        # )
-        # self.add_event(
-        #     "system.wifi.setup.network_selection",
-        #     self.prompt_to_select_network,
-        # )
-        # self.add_event("system.wifi.setup.connected", self.report_setup_complete)
+        self.add_event("hardware.network-not-detected", self.connect_to_wifi)
 
-        # TODO when on screen setup ready - trigger from button push
-        # self.add_event("mycroft.wifi.setup", self.show_all_screens)
-        if has_paired_before():
-            self.log.debug(
-                "Device has previously connected to a network. Delaying Wifi "
-                "to provide system time to connect to slower Wifi networks."
-            )
-            sleep(25)
-        else:
-            # Give the GUI and Wifi Connect time to get started.
-            sleep(5)
-        if not connected():
-            self.connect_to_wifi()
+        self.add_event(
+            "system.wifi.setup.hotspot_activated",
+            self._prompt_to_select_access_point,
+        )
 
-    @intent_handler("test-gui.intent")
-    def test_gui(self, _):
-        """Show GUI screens at a consistent interval.
+        self.add_event(
+            "system.wifi.setup.hotspot_connected",
+            self._prompt_to_select_wifi_network,
+        )
 
-        Testing wifi setup is difficult because the device must be in a certain state
-        for this skill to activate.  This method allows a developer to test changes to
-        the GUI without the difficulty of getting the device in a wifi setup state.
-        """
-        pages = [
-            "access_point_select", "follow_prompt", "network_select", "wifi_success"
-        ]
-        for page in pages:
-            self._show_page(page)
-            sleep(10)
+        self.add_event(
+            "system.wifi.setup.connected", self._report_setup_complete
+        )
+
+    # @intent_handler("test-gui.intent")
+    # def test_gui(self, _):
+    #     """Show GUI screens at a consistent interval.
+
+    #     Testing wifi setup is difficult because the device must be in a certain state
+    #     for this skill to activate.  This method allows a developer to test changes to
+    #     the GUI without the difficulty of getting the device in a wifi setup state.
+    #     """
+    #     pages = [
+    #         "access_point_select", "follow_prompt", "network_select", "wifi_success"
+    #     ]
+    #     for page in pages:
+    #         self._show_page(page)
+    #         sleep(10)
 
     def connect_to_wifi(self):
         """Connect the device to a wifi network.
@@ -98,33 +94,134 @@ class WifiConnect(MycroftSkill):
         communicating with Selene.  If a device is already connected to the internet,
         this setup logic should be skipped.
         """
-        step_timeout = 8
-        self.schedule_repeating_event(
-            self._check_for_internet_connection,
-            when=None,
-            frequency=2,
-            name="InternetConnectCheck"
+        # Create a separate thread of DBus interaction
+        async_thread = threading.Thread(
+            target=self._connect_to_wifi_async, daemon=True
         )
-        steps = [
-            self._prompt_to_select_access_point,
-            self._prompt_to_select_wifi_network,
-            self._display_select_wifi_network,
-        ]
-        for step in steps:
-            if self.connected_to_internet:
-                break
-            step()
-            sleep_duration = 0
-            while not self.connected_to_internet and sleep_duration < step_timeout:
-                sleep(1)
-                sleep_duration += 1
+        async_thread.start()
 
-        while not self.connected_to_internet:
-            self.log.debug("No internet connection detected, waiting...")
-            sleep(2)
+    def _connect_to_wifi_async(self):
+        """Create new event loop and run DBus code"""
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self._dbus_async())
+        loop.close()
 
-        self.cancel_scheduled_event("InternetConnectCheck")
-        self._report_setup_complete()
+    async def _dbus_async(self):
+        try:
+            self.log.info("Wi-Fi connection process started")
+            self.bus.emit(Message("system.wifi.setup.started"))
+
+            dbus = get_dbus()
+            await dbus.connect()
+
+            nm_interface = await get_network_manager(dbus)
+
+            # Find the first wi-fi device
+            wifi_wireless_interface = None
+            wifi_props_interface = None
+
+            for device_path in await nm_interface.get_all_devices():
+                dev_introspect = await dbus.introspect(
+                    NM_NAMESPACE, device_path
+                )
+                dev_object = dbus.get_proxy_object(
+                    NM_NAMESPACE, device_path, dev_introspect
+                )
+
+                dev_interface = dev_object.get_interface(
+                    f"{NM_NAMESPACE}.Device",
+                )
+
+                dev_type = await dev_interface.get_device_type()
+
+                if dev_type == NM_DEVICE_TYPE_WIFI:
+                    self.log.debug("Wi-Fi device found: %s", device_path)
+
+                    # Get a Device.Wireless interface to get at mode, etc.
+                    # https://developer-old.gnome.org/NetworkManager/stable/nm-dbus-types.html#NM80211Mode
+                    wifi_wireless_interface = dev_object.get_interface(
+                        f"{NM_NAMESPACE}.Device.Wireless",
+                    )
+
+                    # Get access to PropertiesChanged signal
+                    wifi_props_interface = dev_object.get_interface(
+                        "org.freedesktop.DBus.Properties"
+                    )
+                    break
+
+            assert (
+                wifi_wireless_interface is not None
+            ), "No Wi-Fi interface found"
+
+            # Subscribe to changes in the wi-fi device properties.
+            # We're interested in mode.
+            mode_ready = asyncio.Event()
+            wifi_mode = NM_802_11_MODE_UNKNOWN
+
+            def properties_changed(
+                interface, changed_props, _invalidated_props
+            ):
+                nonlocal wifi_mode
+
+                if (
+                    interface
+                    == "org.freedesktop.NetworkManager.Device.Wireless"
+                ) and ("Mode" in changed_props):
+                    wifi_mode = changed_props["Mode"].value
+                    mode_ready.set()
+
+            wifi_props_interface.on_properties_changed(properties_changed)
+
+            self.log.info("Waiting for Mycroft access point to activate")
+
+            # Get current mode
+            wifi_mode = await wifi_wireless_interface.get_mode()
+
+            while wifi_mode != NM_802_11_MODE_AP:
+                # Wait for access point to be active
+                await mode_ready.wait()
+
+            self.log.info("Mycroft access point activated")
+
+            # Mycroft access point is active
+            self.bus.emit(Message("system.wifi.setup.hotspot_activated"))
+
+            # TODO: Any way to detect connection from client?
+            await asyncio.sleep(10)
+
+            # User has connected to access point
+            self.bus.emit(Message("system.wifi.setup.hotspot_connected"))
+
+            # TODO: Any way to detect viewing of portal?
+            await asyncio.sleep(5)
+
+            self.bus.emit(Message("system.wifi.setup.network_selection"))
+
+            # Now wait for access point to go away (user has entered Wi-Fi
+            # credentials).
+            mode_ready.clear()
+            self.log.info("Waiting for Mycroft access point to deactivate")
+
+            wifi_mode = await wifi_wireless_interface.get_mode()
+            while wifi_mode == NM_802_11_MODE_AP:
+                # Wait for access point to be deactivated
+                await mode_ready.wait()
+
+            self.log.info("Mycroft access point deactivated")
+
+            # Clean up
+            wifi_props_interface.off_properties_changed(properties_changed)
+            await dbus.wait_for_disconnect()
+
+            self.bus.emit(Message("system.wifi.setup.connected"))
+        except Exception as error:
+            self.log.exception("error while connecting to wi-fi")
+            self.bus.emit(
+                Message("system.wifi.setup.error", data={"error": str(error)})
+            )
+
+        self.bus.emit(Message("system.wifi.setup.ended"))
 
     def _prompt_to_select_access_point(self):
         """Prompt user to join temporary access point."""
@@ -141,19 +238,13 @@ class WifiConnect(MycroftSkill):
         """Prompt user to select network and login."""
         self._show_page("network_select")
 
-    def _check_for_internet_connection(self):
-        """Determine if the device connected successfully."""
-        self.connected_to_internet = connected()
-        if self.connected_to_internet:
-            stop_speaking()
-
     def _report_setup_complete(self):
         """Report when wifi setup is complete, network is connected."""
         self.gui["label"] = self.translate("connected")
         self._show_page("wifi_success")
         sleep(5)
         self.gui.release()
-        self.bus.emit(Message('mycroft.ready'))
+        self.bus.emit(Message("mycroft.ready"))
 
     def _show_page(self, page_name_prefix: str):
         """Shows the appropriate screen for the device's platform.
