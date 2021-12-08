72d94ca85ea67fc15d901611d51431254be039c5
Michael Hansen <mike@rhasspy.org>
2021-12-08 15:46:09 -0500

    Clean up dbus code

--- __init__.py
+++ __init__.py
@@ -34,7 +34,6 @@ MARK_II = "mycroft_mark_2"
 NM_DEVICE_TYPE_WIFI = 2
 
 NM_802_11_MODE_UNKNOWN = 0
-NM_802_11_MODE_INFRA = 2
 NM_802_11_MODE_AP = 3
 
 
@@ -56,7 +55,6 @@ class WifiConnect(MycroftSkill):
 
     def initialize(self):
         """Create event handlers"""
-        self.add_event("hardware.network-not-detected", self.connect_to_wifi)
 
         self.add_event(
             "system.wifi.setup.hotspot_activated",
@@ -72,20 +70,8 @@ class WifiConnect(MycroftSkill):
             "system.wifi.setup.connected", self._report_setup_complete
         )
 
-    # @intent_handler("test-gui.intent")
-    # def test_gui(self, _):
-    #     """Show GUI screens at a consistent interval.
-
-    #     Testing wifi setup is difficult because the device must be in a certain state
-    #     for this skill to activate.  This method allows a developer to test changes to
-    #     the GUI without the difficulty of getting the device in a wifi setup state.
-    #     """
-    #     pages = [
-    #         "access_point_select", "follow_prompt", "network_select", "wifi_success"
-    #     ]
-    #     for page in pages:
-    #         self._show_page(page)
-    #         sleep(10)
+        # Start skill logic only if network not connected
+        self.add_event("hardware.network-not-detected", self.connect_to_wifi)
 
     def connect_to_wifi(self):
         """Connect the device to a wifi network.
@@ -115,106 +101,61 @@ class WifiConnect(MycroftSkill):
             dbus = get_dbus()
             await dbus.connect()
 
-            nm_interface = await get_network_manager(dbus)
+            _nm_object, nm_interface = await get_network_manager(dbus)
 
-            # Find the first wi-fi device
-            wifi_wireless_interface = None
-            wifi_props_interface = None
+            (
+                wifi_props_interface,
+                wifi_wireless_interface,
+            ) = await self._get_wireless_device(dbus, nm_interface)
 
-            for device_path in await nm_interface.get_all_devices():
-                dev_introspect = await dbus.introspect(
-                    NM_NAMESPACE, device_path
-                )
-                dev_object = dbus.get_proxy_object(
-                    NM_NAMESPACE, device_path, dev_introspect
-                )
-
-                dev_interface = dev_object.get_interface(
-                    f"{NM_NAMESPACE}.Device",
-                )
-
-                dev_type = await dev_interface.get_device_type()
-
-                if dev_type == NM_DEVICE_TYPE_WIFI:
-                    self.log.debug("Wi-Fi device found: %s", device_path)
-
-                    # Get a Device.Wireless interface to get at mode, etc.
-                    # https://developer-old.gnome.org/NetworkManager/stable/nm-dbus-types.html#NM80211Mode
-                    wifi_wireless_interface = dev_object.get_interface(
-                        f"{NM_NAMESPACE}.Device.Wireless",
-                    )
-
-                    # Get access to PropertiesChanged signal
-                    wifi_props_interface = dev_object.get_interface(
-                        "org.freedesktop.DBus.Properties"
-                    )
-                    break
-
-            assert (
+            assert (wifi_props_interface is not None) and (
                 wifi_wireless_interface is not None
-            ), "No Wi-Fi interface found"
+            ), "No Wi-Fi device found"
 
-            # Subscribe to changes in the wi-fi device properties.
-            # We're interested in mode.
-            mode_ready = asyncio.Event()
-            wifi_mode = NM_802_11_MODE_UNKNOWN
+            async def is_hotspot_up():
+                wifi_mode = await wifi_wireless_interface.get_mode()
+                return wifi_mode == NM_802_11_MODE_AP
+
+            # Subscribe to changes in the wifi device properties
+            props_changed = asyncio.Event()
 
             def properties_changed(
-                interface, changed_props, _invalidated_props
+                _interface, _changed_props, _invalidated_props
             ):
-                nonlocal wifi_mode
-
-                if (
-                    interface
-                    == "org.freedesktop.NetworkManager.Device.Wireless"
-                ) and ("Mode" in changed_props):
-                    wifi_mode = changed_props["Mode"].value
-                    mode_ready.set()
+                props_changed.set()
 
             wifi_props_interface.on_properties_changed(properties_changed)
 
-            self.log.info("Waiting for Mycroft access point to activate")
+            # Wait for hotspot to be active
+            self.log.info("Waiting for Mycroft hotspot to activate")
+            while not (await is_hotspot_up()):
+                await props_changed.wait()
 
-            # Get current mode
-            wifi_mode = await wifi_wireless_interface.get_mode()
+            self.log.info("Mycroft hotspot activated")
 
-            while wifi_mode != NM_802_11_MODE_AP:
-                # Wait for access point to be active
-                await mode_ready.wait()
-
-            self.log.info("Mycroft access point activated")
-
-            # Mycroft access point is active
+            # Mycroft hotspot is active
             self.bus.emit(Message("system.wifi.setup.hotspot_activated"))
 
             # TODO: Any way to detect connection from client?
             await asyncio.sleep(10)
 
-            # User has connected to access point
+            # User has connected to hotspot
             self.bus.emit(Message("system.wifi.setup.hotspot_connected"))
 
-            # TODO: Any way to detect viewing of portal?
-            await asyncio.sleep(5)
+            # Now wait for hotspot to go away (user has entered # credentials)
+            self.log.info("Waiting for Mycroft hotspot to deactivate")
+            props_changed.clear()
+            while (await is_hotspot_up()):
+                await props_changed.wait()
+                props_changed.clear()
 
-            self.bus.emit(Message("system.wifi.setup.network_selection"))
+            self.log.info("Mycroft hotspot deactivated")
 
-            # Now wait for access point to go away (user has entered Wi-Fi
-            # credentials).
-            mode_ready.clear()
-            self.log.info("Waiting for Mycroft access point to deactivate")
-
-            wifi_mode = await wifi_wireless_interface.get_mode()
-            while wifi_mode == NM_802_11_MODE_AP:
-                # Wait for access point to be deactivated
-                await mode_ready.wait()
-
-            self.log.info("Mycroft access point deactivated")
+            self.bus.emit(Message("system.wifi.setup.connected"))
 
             # Clean up
             wifi_props_interface.off_properties_changed(properties_changed)
             await dbus.wait_for_disconnect()
-
-            self.bus.emit(Message("system.wifi.setup.connected"))
         except Exception as error:
             self.log.exception("error while connecting to wi-fi")
             self.bus.emit(
@@ -222,6 +163,40 @@ class WifiConnect(MycroftSkill):
             )
 
         self.bus.emit(Message("system.wifi.setup.ended"))
+
+    async def _get_wireless_device(self, dbus, nm_interface):
+        """Return props/wireless interfaces for first wifi device"""
+
+        # Find the first wi-fi device
+        wifi_wireless_interface = None
+        wifi_props_interface = None
+
+        for device_path in await nm_interface.get_all_devices():
+            dev_introspect = await dbus.introspect(NM_NAMESPACE, device_path)
+            dev_object = dbus.get_proxy_object(
+                NM_NAMESPACE, device_path, dev_introspect
+            )
+
+            dev_interface = dev_object.get_interface(f"{NM_NAMESPACE}.Device")
+
+            dev_type = await dev_interface.get_device_type()
+
+            if dev_type == NM_DEVICE_TYPE_WIFI:
+                self.log.debug("Wi-Fi device found: %s", device_path)
+
+                # Get a Device.Wireless interface to get at mode, etc.
+                # https://developer-old.gnome.org/NetworkManager/stable/nm-dbus-types.html#NM80211Mode
+                wifi_wireless_interface = dev_object.get_interface(
+                    f"{NM_NAMESPACE}.Device.Wireless",
+                )
+
+                # Get access to PropertiesChanged signal
+                wifi_props_interface = dev_object.get_interface(
+                    "org.freedesktop.DBus.Properties"
+                )
+                break
+
+        return wifi_props_interface, wifi_wireless_interface
 
     def _prompt_to_select_access_point(self):
         """Prompt user to join temporary access point."""
