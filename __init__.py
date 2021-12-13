c25a96fb199dcdf3c83db7c57e371ba022aab778
Michael Hansen <mike@rhasspy.org>
2021-12-13 11:16:57 -0500

    Show connecting page when user selects access point

--- __init__.py
+++ __init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 #
 """Mycroft skill for joining a device to a WiFi network."""
+from enum import Enum
 from time import sleep
 
 from mycroft.audio import stop_speaking
@@ -22,6 +23,16 @@ from mycroft.skills import MycroftSkill, intent_handler
 
 
 MARK_II = "mycroft_mark_2"
+
+
+class State(str, Enum):
+    """State of the wifi-connect skill"""
+
+    IDLE = "idle"
+    ACTIVATING_HOTSPOT = "activating-hotspot"
+    WAITING_FOR_PASSWORD = "waiting-for-password"
+    CONNECTING_TO_WIFI = "connecting-to-wifi"
+
 
 class WifiConnect(MycroftSkill):
     """Skill that joins a device to a WiFi network.
@@ -33,7 +44,8 @@ class WifiConnect(MycroftSkill):
     def __init__(self):
         super().__init__()
         self.page_showing = None
-        self.connected_to_internet = False
+
+        self.state: State = State.IDLE
 
     @property
     def platform(self):
@@ -41,24 +53,117 @@ class WifiConnect(MycroftSkill):
 
     def initialize(self):
         """Create event handlers"""
+
+        # Trigger condition for skill
+        self.add_event(
+            "hardware.network-not-detected", self._handle_network_not_detected
+        )
+
+        self.add_event("system.wifi.setup.started", self._handle_started)
+
+        # Hotspot has been created
         self.add_event(
             "system.wifi.setup.hotspot-activated",
-            self._prompt_to_select_access_point,
+            self._handle_hotspot_activated,
         )
 
+        # User has connected to portal page
         self.add_event(
             "system.wifi.setup.hotspot-connected",
-            self._prompt_to_select_wifi_network,
+            self._handle_hotspot_connected,
         )
 
+        # User has selected an access point
         self.add_event(
-            "system.wifi.setup.connected", self._report_setup_complete
+            "system.wifi.setup.hotspot-selected",
+            self._handle_hotspot_selected,
         )
+
+        # Hotspot removed so wifi connection can be attempted
+        self.add_event(
+            "system.wifi.setup.hotspot-deactivated",
+            self._handle_hotspot_deactivated,
+        )
+
+        # Error condition
+        self.add_event("system.wifi.setup.failed", self._handle_setup_failed)
+
+        # Success condition for skill
+        self.add_event(
+            "hardware.network-detected", self._handle_network_detected
+        )
+
+        # Setup is complete
+        self.add_event(
+            "system.wifi.setup.connected", self._handle_setup_connected
+        )
+
+    # -------------------------------------------------------------------------
+
+    def _handle_network_not_detected(self, _message=None):
+        if self.state == State.IDLE:
+            # First run of the skill
+            self.bus.emit(Message("system.wifi.setup.started"))
+        elif self.state == State.CONNECTING_TO_WIFI:
+            # WiFi connection attempt failed
+            self.bus.emit(Message("system.wifi.setup.failed"))
+
+    def _handle_started(self, _message=None):
+        if self.state == State.IDLE:
+            self.state = State.ACTIVATING_HOTSPOT
+            self.bus.emit(Message("system.wifi.setup.create-hotspot"))
+
+    def _handle_hotspot_activated(self, _message=None):
+        if self.state == State.ACTIVATING_HOTSPOT:
+            self.state = State.WAITING_FOR_PASSWORD
+            self._prompt_to_select_access_point()
+            self._display_select_wifi_network()
+
+    def _handle_hotspot_selected(self, _message=None):
+        if self.state == State.WAITING_FOR_PASSWORD:
+            self.state = State.CONNECTING_TO_WIFI
+
+            # Inform user that connection is being attempted
+            self._show_connecting_page()
+
+    def _handle_hotspot_deactivated(self, _message=None):
+        if self.state == State.CONNECTING_TO_WIFI:
+            # Request network detection from enclosure
+            self.bus.emit(Message("hardware.detect-network"))
+
+    def _handle_hotspot_connected(self, _message=None):
+        if self.state == State.WAITING_FOR_PASSWORD:
+            self._prompt_to_select_wifi_network()
+
+    def _handle_setup_failed(self, _message=None):
+        self.log.info("Wi-Fi setup failed")
+
+        if self.state == State.CONNECTING_TO_WIFI:
+            self.state = State.IDLE
+            self._show_failure_page()
+
+            # Start over
+            self.bus.emit(Message("system.wifi.setup.started"))
+
+    def _handle_network_detected(self, _message=None):
+        if self.state == State.CONNECTING_TO_WIFI:
+            # WiFi setup succeeded
+            self.bus.emit(Message("system.wifi.setup.connected"))
+
+    def _handle_setup_connected(self, _message=None):
+        self.log.info("Wi-Fi setup succeeded")
+
+        self.state = State.IDLE
+        self.bus.emit(Message("system.wifi.setup.ended"))
+
+        self._report_setup_complete()
+
+    # -------------------------------------------------------------------------
 
     def _prompt_to_select_access_point(self):
         """Prompt user to join temporary access point."""
         self._show_page("access_point_select")
-        self.speak_dialog("access_point_created", wait=True)
+        self.speak_dialog("access-point-created", wait=True)
 
     def _prompt_to_select_wifi_network(self):
         """Prompt user to sign into access point."""
@@ -70,12 +175,23 @@ class WifiConnect(MycroftSkill):
         """Prompt user to select network and login."""
         self._show_page("network_select")
 
+    def _show_connecting_page(self, _message=None):
+        """Inform user that wifi connection is being attempted"""
+        self._show_page("connecting")
+
+    def _show_failure_page(self, _message=None):
+        """Inform user that wifi connection failed"""
+        self._show_page("wifi_failure")
+        self.speak_dialog("wifi-failure", wait=True)
+
     def _report_setup_complete(self):
         """Report when wifi setup is complete, network is connected."""
         self.gui["label"] = self.translate("connected")
         self._show_page("wifi_success")
         sleep(5)
         self.gui.release()
+
+    # -------------------------------------------------------------------------
 
     def _show_page(self, page_name_prefix: str):
         """Shows the appropriate screen for the device's platform.
